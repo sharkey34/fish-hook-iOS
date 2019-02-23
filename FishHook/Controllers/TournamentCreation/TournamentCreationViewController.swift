b722e4426068358d32174e30a427ef321843ca9b
Eric Sharkey <eisharkey@student.fullsail.edu>
2019-02-22 20:43:45 -0500

    ADD - tab view controller and split view controller

---
+++ FishHook/Controllers/TournamentCreation/TournamentCreationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TournamentCreationViewController.swift
+//  FishHook
+//
+//  Created by Eric Sharkey on 2/22/19.
+//  Copyright © 2019 Eric Sharkey. All rights reserved.
+//
+
+import UIKit
+
+class TournamentCreationViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
