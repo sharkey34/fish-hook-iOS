ccdedb72ba4dfcba7325d13ce18bd342d5554f2c
Eric Sharkey <eisharkey@student.fullsail.edu>
2019-02-27 20:58:17 -0500

    ADD - initial logic to customize the calendar

--- FishHook/TournamentCreation/Controllers/RunDatesVC.swift
+++ FishHook/TournamentCreation/Controllers/RunDatesVC.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import FSCalendar
 
 class RunDatesVC: UIViewController {
+    @IBOutlet weak var calendar: FSCalendar!
     
 
     override func viewDidLoad() {
@@ -17,5 +19,35 @@ class RunDatesVC: UIViewController {
         navigationController?.navigationBar.barTintColor = UIColor(displayP3Red: 13/255, green: 102/255, blue: 163/255, alpha: 1)
         navigationItem.title = TournamentSetup.Dates.rawValue
         
+        setUpCalendar()
+        
+    }
+    
+    func setUpCalendar(){
+        calendar.delegate = self
+        calendar.dataSource = self
+        calendar.allowsMultipleSelection = true
+        calendar.swipeToChooseGesture.isEnabled = true
+        
+        // NO idea what is going on
+//        calendar.swipeToChooseGesture.isEnabled = true // Swipe-To-Choose
+        let scopeGesture = UIPanGestureRecognizer(target: calendar, action: #selector(calendar.handleScopeGesture(_:)));
+        calendar.addGestureRecognizer(scopeGesture)
+//        calendar.register(DIYCalendarCell.self, forCellReuseIdentifier: "cell")
+        
+
+    }
+}
+
+extension RunDatesVC: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
+    
+    // Datasource
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        // TODO: Change for AutoLayout
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        // TODO: Get the date selected.
+//        self.configureVisibleCells()
     }
 }
